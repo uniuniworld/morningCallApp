c2d9c7d7faa4edac890ef383c6dad92db51dadec
t_kirifu <uniuniworld777@gmail.comu>
2020-03-15 15:26:24 +0900

    プロジェクト作成時

---
+++ morningCall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  morningCall
+//
+//  Created by Takahiro Kirifu on 2020/03/15.
+//  Copyright © 2020 Takahiro Kirifu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
